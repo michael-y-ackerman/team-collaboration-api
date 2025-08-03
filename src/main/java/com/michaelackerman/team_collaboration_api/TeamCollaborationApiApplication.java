38c9b7659267f15d96a7d67bb21e1d99f0c20755
michael-y-ackerman <michaelackerman314@gmail.com>
2025-08-02 23:31:51 -0400

    initial commit

---
+++ src/main/java/com/michaelackerman/team_collaboration_api/TeamCollaborationApiApplication.java
@@ -0,0 +1,13 @@
+package com.michaelackerman.team_collaboration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCollaborationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCollaborationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelackerman/team_collaboration_api/TeamCollaborationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelackerman.team_collaboration_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamCollaborationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
